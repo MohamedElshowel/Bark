434809e33cd59b3d2252e9e09cd416d3a469443d
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2020-03-24 18:31:23 +0200

    Handle responsivness in mobile view

--- modules/ProductsHandler.js
+++ modules/ProductsHandler.js
@@ -27,28 +27,6 @@ function initProducts() {
  */
 function _initProductsCarousel() {
     $('.best-sellers--products').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
+        slidesToShow: (window.innerWidth > 720) ? 3 : 1,
     });
 }
--- modules/SignUpHandler.js
+++ modules/SignUpHandler.js
@@ -5,31 +5,29 @@ const animationTime = 300;
  */
 function bindSignUpActions() {
     // Sign Up Button in Landing Page
-    $("#landing--sign-up-btn").click(() => {
-        $("#sign-up-component").css("height", 0);
-        $("#sign-up-component").show();
-        $("#sign-up-component").animate({
-            height: "100%"
-        }, animationTime);
-    });
-
+    $("#landing--sign-up-btn").click(openSignUpForm);
+    // Sign Up Button in Footer - Contact
+    $(".links--contact--sign-up").click(openSignUpForm);
     // Close Button in the Sign Up Pop-Up
-    $(".sign-up--controller--close-btn").click(() => {
-        $("#sign-up-component").animate({
-            height: 0
-        }, animationTime, () => {
-            $("#sign-up-component").hide();
-            $("#sign-up-component").css("height", "100%");
-        });
-    });
-
+    $(".sign-up--close-btn").click(closeSignUpForm);
     // Sign Up Button in the Sign Up Pop-Up
-    $(".sign-up--controller--btn").click(() => {
-        $("#sign-up-component").animate({
-            height: 0,
-        }, animationTime, () => {
-            $("#sign-up-component").hide();
-            $("#sign-up-component").css("height", "100%");
-        });
+    $(".sign-up--controller--btn").click(closeSignUpForm);
+}
+
+
+function openSignUpForm() {
+    $("#sign-up-component").css("height", 0);
+    $("#sign-up-component").show();
+    $("#sign-up-component").animate({
+        height: "100%"
+    }, animationTime);
+}
+
+function closeSignUpForm() {
+    $("#sign-up-component").animate({
+        height: 0,
+    }, animationTime, () => {
+        $("#sign-up-component").hide();
+        $("#sign-up-component").css("height", "100%");
     });
 }
